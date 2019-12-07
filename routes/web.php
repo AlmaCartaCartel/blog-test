bd241148377882dc641809101ca20a65991f0e8f
AlmaCartaCartel <karitanjecksparov@gmail.com>
2019-12-07 21:35:20 +0300

    start blog create

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,7 @@
 */
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
     Route::get('/', 'DashboardController@index');
